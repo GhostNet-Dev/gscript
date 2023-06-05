281670e0d97ee3b54016c004227499e7513eb1c9
zcbm4321 <zcbm4321@gmail.com>
2023-06-05 18:01:03 +0900

    compiler & virtual machine

--- repl/repl.go
+++ repl/repl.go
@@ -5,15 +5,55 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/GhostNet-Dev/glambda/compiler"
 	"github.com/GhostNet-Dev/glambda/evaluator"
 	"github.com/GhostNet-Dev/glambda/lexer"
 	"github.com/GhostNet-Dev/glambda/object"
 	"github.com/GhostNet-Dev/glambda/parser"
+	"github.com/GhostNet-Dev/glambda/vm"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lexer := lexer.NewLexer(line)
+		p := parser.NewParser(lexer)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		comp := compiler.NewCompiler()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.NewVM(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+		stackTop := machine.LastPoppedStackElem()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+func StartEval(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
 
---
+++ vm/vm_test.go
@@ -0,0 +1,104 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/GhostNet-Dev/glambda/ast"
+	"github.com/GhostNet-Dev/glambda/compiler"
+	"github.com/GhostNet-Dev/glambda/lexer"
+	"github.com/GhostNet-Dev/glambda/object"
+	"github.com/GhostNet-Dev/glambda/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+	}
+	runVmTests(t, tests)
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 3},
+		{"1 - 2", -1},
+		{"4 / 2", 2},
+		{"50 / 2 * 2 + 10 - 5", 55},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"5 * (2 + 10)", 60},
+	}
+	runVmTests(t, tests)
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+	for _, tt := range tests {
+		program := parse(tt.input)
+		comp := compiler.NewCompiler()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+		vm := NewVM(comp.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+		stackElem := vm.LastPoppedStackElem()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	case bool:
+		err := testBooleanObject(bool(expected), actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	return p.ParseProgram()
+}
+
+func testBooleanObject(expected bool, actual object.Object) error {
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)", actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+	return nil
+}
