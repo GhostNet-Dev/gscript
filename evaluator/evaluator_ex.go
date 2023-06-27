2d8a8f567c7f80cf937e83795bf6cfa530338da1
zcbm4321 <zcbm4321@gmail.com>
2023-06-27 11:10:35 +0900

    newfile

---
+++ evaluator/evaluator_ex.go
@@ -0,0 +1,42 @@
+package evaluator
+
+import (
+	"github.com/GhostNet-Dev/glambda/ast"
+	"github.com/GhostNet-Dev/glambda/object"
+)
+
+func evalTypeExpression(node *ast.TypeStatement, env *object.Environment) object.Object {
+	name := node.Name.Value
+
+	switch node.Type.Value {
+	case "struct":
+		structEnv := env.TypeDefine(name)
+		return &object.Struct{Name: name, Value: Eval(node.Body, structEnv)}
+	}
+
+	return nil
+}
+
+func evalObjectBlockStatement(block *ast.ObjectBlockStatement, env *object.Environment) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement, env)
+	}
+	return result
+}
+
+func evalForExpression(ie *ast.ForExpression, env *object.Environment) object.Object {
+	Eval(ie.Init, env)
+	condition := Eval(ie.Condition, env)
+	if isError(condition) {
+		return condition
+	}
+
+	for isTruthy(condition) {
+		Eval(ie.Consequence, env)
+		Eval(ie.Increment, env)
+		condition = Eval(ie.Condition, env)
+	}
+	return NULL
+}
